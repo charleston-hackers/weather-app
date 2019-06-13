0c1f86d2e102a30d7e4c9154a7f9213b83d0ee94
Steven Thomason <thomason84@hotmail.com>
2019-06-12 21:25:00 -0400

    added express server

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,14 @@
 
 
 <script>
-import NavBar from '@/components/NavBar';
-import Footer from '@/components/Footer';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
     NavBar,
     Footer
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-    }
   }
 }
 </script>
