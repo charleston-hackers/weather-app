4b070e81f2c0273bb59c234a53ad55ebcd8543e0
Steven <steven@vetfriends.com>
2019-06-14 13:28:42 -0400

    fixing 404 route issue

--- src/router/index.js
+++ src/router/index.js
@@ -10,30 +10,17 @@ export default new Router({
     {
       path: '/',
       name: 'CurrentForecast',
-      component: CurrentForecast,
-      meta: {
-        title: 'Current Weather | Local forecast by zip code',
-        metaTags: [
-          {
-            name: 'description',
-            content: 'Get the current weather forecast by zip code.'
-          }
-        ]
-      }
+      component: CurrentForecast
     },
     {
       path: '/5-Day-Forecast',
       name: 'FiveDayForecast',
-      component: FiveDayForecast,
-      meta: {
-        title: '5 Day Forecast | Local 5 day forecast by zip code',
-        metaTags: [
-          {
-            name: 'description',
-            content: 'Get 5 day weather forecast by zip code.'
-          }
-        ]
-      }
+      component: FiveDayForecast
+    },
+    {
+      path: '*',
+      component: CurrentForecast
     }
-  ]
+  ],
+    mode: 'history'
 })
